5b5c87a4779a94ae86439b664a02c812ebb4bf59
Igor Braun <igorbr1991@gmail.com>
2020-07-08 17:09:33 +0200

    refactoring

---
+++ modules/paxassign/include/motis/paxassign/capacitaty_aware_structs.h
@@ -0,0 +1,33 @@
+#pragma once
+
+namespace motis::paxassign {
+
+enum class edge_type : std::uint8_t { TRIP, INTERCHANGE, WAIT, NOROUTE };
+
+struct cap_ILP_edge {
+  uint32_t id_;
+  double tt_;
+  int capacity_;
+  edge_type type_;
+};
+
+struct cap_ILP_connection {
+  uint32_t id_;
+  std::vector<cap_ILP_edge> edges_;
+};
+
+struct cap_ILP_psg_group {
+  uint32_t id_;
+  std::vector<cap_ILP_connection> alternatives_;
+  int psg_count_;
+};
+
+struct cap_ILP_config {
+  std::vector<double> cost_function_capacity_steps_{0.65, 1.0, 1.2};
+  std::vector<double> tt_and_waiting_penalties_{1.0, 1.2, 2.0};
+
+  double const transfer_penalty_ = 5;
+  double const no_route_cost_ = 10000;
+};
+
+}  // namespace motis::paxassign
