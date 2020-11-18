1298e65d6a503d839b4299df51f5640d8c2b9e6b
Igor Braun <igorbr1991@gmail.com>
2020-11-18 11:22:07 +0100

    only shortest path based on travel time (not perceived travel time) for greedy algorithm (as in the Halle Paper)

--- modules/paxassign/include/motis/paxassign/heuristic_algo/greedy.h
+++ modules/paxassign/include/motis/paxassign/heuristic_algo/greedy.h
@@ -16,29 +16,21 @@ std::vector<std::vector<eg_edge*>> greedy_assignment(
   std::iota(std::begin(psgs_indices), std::end(psgs_indices), 0);
   std::shuffle(std::begin(psgs_indices), std::end(psgs_indices), rng);
 
-  auto calc_perc_tt_dist = [&](eg_edge* e, double curr_dist) {
+  auto calc_tt_dist = [&](eg_edge* e, double curr_dist) {
     if (e->capacity_utilization_ >
         config.cost_function_capacity_steps_.back()) {
       return (double)config.no_route_cost_ + curr_dist;
     }
-    double transfer_penalty = (e->type_ == eg_edge_type::TRAIN_ENTRY)
-                                  ? config.transfer_penalty_
-                                  : 0.0;
-    auto const it = std::lower_bound(
-        config.cost_function_capacity_steps_.begin(),
-        config.cost_function_capacity_steps_.end(), e->capacity_utilization_);
-    auto idx = std::distance(config.cost_function_capacity_steps_.begin(), it);
-    return config.tt_and_waiting_penalties_[idx] * e->cost_ + transfer_penalty +
-           curr_dist;
+    return e->cost_ + curr_dist;
   };
 
   for (auto i = 0u; i < psgs_indices.size(); ++i) {
     {
       logging::scoped_timer greedy{"greedy algorithm"};
-      solution[psgs_indices[i]] = sssd_dijkstra<double>(
-          eg_psg_groups[psgs_indices[i]], 0.0,
-          std::numeric_limits<double>::max(), te_graph,
-          nodes_validity[psgs_indices[i]], calc_perc_tt_dist);
+      solution[psgs_indices[i]] =
+          sssd_dijkstra<double>(eg_psg_groups[psgs_indices[i]], 0.0,
+                                std::numeric_limits<double>::max(), te_graph,
+                                nodes_validity[psgs_indices[i]], calc_tt_dist);
       add_psgs_to_edges(solution[psgs_indices[i]],
                         eg_psg_groups[psgs_indices[i]]);
     }
