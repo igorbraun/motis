290b345cfeab584a1dea23254039f824a0be332e
Igor Braun <igorbr1991@gmail.com>
2020-11-25 16:45:38 +0100

    greedy shortest path considers max interchanges number

--- modules/paxassign/include/motis/paxassign/dijkstra.h
+++ modules/paxassign/include/motis/paxassign/dijkstra.h
@@ -60,27 +60,28 @@ std::vector<eg_edge*> sssd_dijkstra(eg_psg_group const& pg,
                                     T const dist_vec_init,
                                     time_expanded_graph const& te_graph,
                                     std::vector<bool> const& nodes_validity,
+                                    int const max_interchanges,
                                     F calc_new_dist) {
   std::map<eg_event_node*, eg_edge*> node_to_incoming_e;
-  typedef std::pair<eg_event_node*, T> node_weight;
+  typedef std::tuple<eg_event_node*, T, int> node_weight;
   auto cmp_min = [](node_weight left, node_weight right) {
-    return left.second > right.second;
+    return std::get<1>(left) > std::get<1>(right);
   };
   std::priority_queue<node_weight, std::vector<node_weight>, decltype(cmp_min)>
       pq(cmp_min);
   std::vector<T> dist(te_graph.nodes_.size(), dist_vec_init);
 
-  pq.push(std::make_pair(pg.from_, start_node_init));
+  pq.push(std::make_tuple(pg.from_, start_node_init, 0));
   dist[pg.from_->id_] = start_node_init;
 
   while (!pq.empty()) {
-    auto curr_node = pq.top().first;
-    auto curr_dist = pq.top().second;
+    auto [curr_node, curr_dist, curr_interchanges] = pq.top();
     pq.pop();
     if (curr_node == pg.to_) {
       break;
     }
     for (auto const& oe : curr_node->out_edges_) {
+      if (curr_interchanges > max_interchanges) continue;
       if (!nodes_validity[oe->to_->id_]) {
         continue;
       }
@@ -88,7 +89,10 @@ std::vector<eg_edge*> sssd_dijkstra(eg_psg_group const& pg,
       auto new_dist = calc_new_dist(oe.get(), curr_dist);
       if (new_dist < dist[oe->to_->id_]) {
         dist[oe->to_->id_] = new_dist;
-        pq.push(std::make_pair(oe->to_, new_dist));
+        auto new_interchanges = (oe->type_ == eg_edge_type::TRAIN_ENTRY)
+                                    ? curr_interchanges + 1
+                                    : curr_interchanges;
+        pq.push(std::make_tuple(oe->to_, new_dist, new_interchanges));
         node_to_incoming_e[oe->to_] = oe.get();
       }
     }
--- modules/paxassign/include/motis/paxassign/heuristic_algo/greedy.h
+++ modules/paxassign/include/motis/paxassign/heuristic_algo/greedy.h
@@ -5,32 +5,26 @@
 namespace motis::paxassign {
 
 // RE-IMPLEMENTATION OF GREEDY ASSIGNMENT AS IN THE HALLE-PAPER
+// TO GET RESULT AS IN THE HALLE-PAPER USE calc_tt_dist as objectve
+template <typename F>
 std::vector<std::vector<eg_edge*>> greedy_assignment(
     time_expanded_graph const& te_graph,
     std::vector<std::vector<bool>> const& nodes_validity,
-    std::vector<eg_psg_group> const& eg_psg_groups,
-    perceived_tt_config const& config, std::mt19937& rng) {
+    int const max_interchanges, std::vector<eg_psg_group> const& eg_psg_groups,
+    std::mt19937& rng, F obj_f) {
   std::vector<std::vector<eg_edge*>> solution(eg_psg_groups.size());
 
   std::vector<int> psgs_indices(eg_psg_groups.size());
   std::iota(std::begin(psgs_indices), std::end(psgs_indices), 0);
   std::shuffle(std::begin(psgs_indices), std::end(psgs_indices), rng);
 
-  auto calc_tt_dist = [&](eg_edge* e, double curr_dist) {
-    if (e->capacity_utilization_ >
-        config.cost_function_capacity_steps_.back()) {
-      return (double)config.no_route_cost_ + curr_dist;
-    }
-    return e->cost_ + curr_dist;
-  };
-
   for (auto i = 0u; i < psgs_indices.size(); ++i) {
     {
       logging::scoped_timer greedy{"greedy algorithm"};
-      solution[psgs_indices[i]] =
-          sssd_dijkstra<double>(eg_psg_groups[psgs_indices[i]], 0.0,
-                                std::numeric_limits<double>::max(), te_graph,
-                                nodes_validity[psgs_indices[i]], calc_tt_dist);
+      solution[psgs_indices[i]] = sssd_dijkstra<double>(
+          eg_psg_groups[psgs_indices[i]], 0.0,
+          std::numeric_limits<double>::max(), te_graph,
+          nodes_validity[psgs_indices[i]], max_interchanges, obj_f);
       add_psgs_to_edges(solution[psgs_indices[i]],
                         eg_psg_groups[psgs_indices[i]]);
     }
