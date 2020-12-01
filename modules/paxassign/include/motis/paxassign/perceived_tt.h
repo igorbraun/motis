1bde9240f91d131074f649991184e047b0e77d64
Igor Braun <igorbr1991@gmail.com>
2020-12-01 21:39:39 +0100

    bugfix & obj calculation after assignment

--- modules/paxassign/include/motis/paxassign/perceived_tt.h
+++ modules/paxassign/include/motis/paxassign/perceived_tt.h
@@ -4,7 +4,7 @@ namespace motis::paxassign {
 
 double calc_perc_tt(std::vector<eg_edge*> const& solution,
                     perceived_tt_config const& perc_tt_config) {
-  double perc_tt;
+  double perc_tt = 0.0;
   for (auto const& e : solution) {
     if (e->type_ == eg_edge_type::NO_ROUTE)
       return perc_tt_config.no_route_cost_;
@@ -33,10 +33,25 @@ double calc_perc_tt_for_scenario(
     perceived_tt_config const& perc_tt_config) {
   double cumulative_perc_tt = 0;
   for (auto i = 0u; i < eg_psg_groups.size(); ++i) {
-    auto curr_sol_cost = calc_perc_tt(scenario_solution[i], perc_tt_config);
-    cumulative_perc_tt += eg_psg_groups[i].psg_count_ * curr_sol_cost;
+    double curr_sol_cost = calc_perc_tt(scenario_solution[i], perc_tt_config);
+    cumulative_perc_tt =
+        cumulative_perc_tt + eg_psg_groups[i].psg_count_ * curr_sol_cost;
   }
   return cumulative_perc_tt;
+}
+
+double get_obj_after_assign(std::vector<eg_psg_group> const& eg_psg_groups,
+                            std::vector<std::vector<eg_edge*>> const& solution,
+                            perceived_tt_config const& perc_tt_config) {
+  for (auto i = 0u; i < eg_psg_groups.size(); ++i) {
+    add_psgs_to_edges(solution[i], eg_psg_groups[i]);
+  }
+  double obj =
+      calc_perc_tt_for_scenario(eg_psg_groups, solution, perc_tt_config);
+  for (auto i = 0u; i < eg_psg_groups.size(); ++i) {
+    remove_psgs_from_edges(solution[i], eg_psg_groups[i]);
+  }
+  return obj;
 }
 
 }  // namespace motis::paxassign
