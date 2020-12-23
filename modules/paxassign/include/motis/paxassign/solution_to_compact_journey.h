c0ebb39771e1cf41cb2208d0849f1a44f57dd4a9
Igor Braun <igorbr1991@gmail.com>
2020-12-23 14:34:00 +0100

    repairing imports

---
+++ modules/paxassign/include/motis/paxassign/solution_to_compact_journey.h
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace motis::paxassign {
+void node_arc_solution_to_compact_j();
+}
---
+++ modules/paxassign/src/solution_to_compact_journey.cc
@@ -0,0 +1,9 @@
+#include "motis/paxassign/solution_to_compact_journey.h"
+
+#include <iostream>
+
+namespace motis::paxassign {
+void node_arc_solution_to_compact_j() {
+  std::cout << "create compact journey from node-arc solution" << std::endl;
+}
+}  // namespace motis::paxassign
